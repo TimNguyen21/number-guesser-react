5d30925216fc153a5cc9f40425d819dff85424df
TimNguyen21 <tim.nguyen2112@gmail.com>
2023-11-25 20:59:23 -0800

    [Main] File Updates
    * Updated package.json
    * Updated file name and import directories: App.js -> NumberGuesser.js

---
+++ src/NumberGuesser.js
@@ -0,0 +1,11 @@
+import './NumberGuesser.scss';
+
+function NumberGuesser() {
+  return (
+    <div className="number-guesser">
+      Hello World!
+    </div>
+  );
+}
+
+export default NumberGuesser;
