b33328f4a9ba684de8536bf1a062f76a81168696
TimNguyen21 <tim.nguyen2112@gmail.com>
2023-11-26 18:45:55 -0800

    [Main] Setup game and results sections

--- src/NumberGuesser.js
+++ src/NumberGuesser.js
@@ -4,8 +4,16 @@ function NumberGuesser() {
   return (
     <div className="number-guesser">
       <section className="number-guesser__header">Number Guesser</section>
+      <section className="number-guesser__settings-section">
+        Settings
+      </section>
       <section className="number-guesser__main">
-        main
+        <section className="number-guesser__game-section">
+          game section
+        </section>
+        <section className="number-guesser__results-section">
+          results section
+        </section>
       </section>
     </div>
   );
