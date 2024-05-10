6179ae34712e9ff1eb511c3d922019b04e6ae34f
TimNguyen21 <tim.nguyen2112@gmail.com>
2024-05-09 21:19:24 -0700

    [main] Updated styling for the Player Card

--- src/containers/game/component/player-card/PlayerCard.js
+++ src/containers/game/component/player-card/PlayerCard.js
@@ -21,14 +21,28 @@ function PlayerCard(props) {
         }
     }
 
+    const summaryGuessColorClassName = (playerNumberGuessResultCode) => {
+        const guessSummaryClassName = 'player-card__guess-summary';
+
+        switch (playerNumberGuessResultCode) {
+            case 1:
+                return `${guessSummaryClassName} player-card__guess-summary-color--won`;
+            case 2:
+            case 3:
+                return `${guessSummaryClassName} player-card__guess-summary-color--incorrect-guess`;
+            default:
+                return '';
+        }
+    }
+
     const errorMessage = (errorCode) => {
         switch (errorCode) {
             case 1:
-                return 'input empty';
+                return 'Cannot have an empty input.  Please input a valid number.';
             case 2:
-                return 'grater than max';
+                return 'Current number guess is higher than the set max. Please guess again.';
             case 3:
-                return 'lower than min';
+                return 'Current number guess is lower than the set min. Please guess again.';
             default:
                 return '';
         }
@@ -49,10 +63,12 @@ function PlayerCard(props) {
                        onChange={getPlayerGuess}
                        value={setValue}/>
             </section>
-            <div className="player-card__guess-summary">
+            <div className={summaryGuessColorClassName(playerNumberGuessResultCode)}>
                 {playerGuessSummary(playerNumberGuessResultCode)}
             </div>
-            {errorCode ? errorMessage(errorCode) : ''}
+            <div className='player-card__guess-error-message'>
+                {errorCode ? errorMessage(errorCode) : ''}
+            </div>
         </div>
     );
 }
